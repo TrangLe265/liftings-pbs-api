608b2853e95c396951fa6a18b175d931ae3055f3
TrangLe265 <trang.le@myy.haaga-helia.fi>
2025-03-31 02:47:27 +0300

    added some endpoints

--- index.js
+++ index.js
@@ -1,8 +1,48 @@
 import express from 'express';
-import supabase from './supabaseClient';
+import supabase from './supabaseClient.js'; 
+import cors from 'cors';
 
-const express = require('express'); 
 const app = express();
+app.use(cors()); 
+app.use(express.json()); 
+
+//middlware to receive and check jwt 
+//user login via app, retrieve the token -> use token to access api
+const authenticateUser = async(req,res,next) => {
+    const token = req.headers.authorization?.split(" ")[1] //extract the header from the token 
+    if (!token) return res.status(401).json({error:"Invalid token, check aiuthorization"}); 
+
+    const {data,error} = await supabase.auth.getUser(token); 
+    if(error || !data?.user) return res.status(401).json({error:"Invalid token, check aiuthorization"}); 
+
+    req.user= data.user; 
+    next(); 
+    //an express.js function
+    //moves from middleware to actual route
+}
+
+app.get("/", async(req,res) =>{
+    res.send("Test")
+})
+
+//to get all lift categories
+app.get("/categories", async (req, res) => {
+    const { data, error } = await supabase
+        .from('lift_category')
+        .select('*'); 
+
+    if (error) return res.status(500).json({ error: error.message });
+    res.json(data);
+});
+
+app.get("/lifts", async (req, res) => {
+    const { data, error } = await supabase
+        .from('lifts')
+        .select('*'); 
+
+    if (error) return res.status(500).json({ error: error.message });
+    res.json(data);
+});
 
 //to get lifts by category
 app.get('/lifts/:lift_catgory_id', async(req,res) => {
@@ -11,3 +51,4 @@ app.get('/lifts/:lift_catgory_id', async(req,res) => {
 
 const port = process.env.PORT || 3000; 
 app.listen(port, () => console.log(`Listening on port ${port}`));
+
